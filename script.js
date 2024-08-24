5628bcbe1a4a286b63b705323ba6caa489eba33b
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-08-24 21:23:05 +0530

    Created functions for calculations and a basic html calculator basically buttons

---
+++ script.js
@@ -0,0 +1,40 @@
+//Variables
+let firstNumber;
+let possibleOperators = ["+", "-", "*", "/"];
+let operator;
+let secondNumber;
+
+//functions for calculations
+function add(param1, param2) {
+  result = param1 + param2;
+  console.log(result);
+}
+function subtract(param1, param2) {
+  result = param1 - param2;
+  console.log(result);
+}
+function multiply(param1, param2) {
+  result = param1 * param2;
+  console.log(result);
+}
+function divide(param1, param2) {
+  result = param1 / param2;
+  console.log(result);
+}
+function operate(firstNumber, operator, secondNumber) {
+  if (possibleOperators.includes(operator)) {
+    if (operator == "+") {
+      add(firstNumber, secondNumber);
+    } else if (operator == "-") {
+      subtract(firstNumber, secondNumber);
+    } else if (operator == "*") {
+      multiply(firstNumber, secondNumber);
+    } else {
+      divide(firstNumber, secondNumber);
+    }
+  } else {
+    console.log("Invalid Operator Found");
+    return;
+  }
+}
+//operate(8, "*", 3);
