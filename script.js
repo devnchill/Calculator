99ec01a42843a85528c879c7f7192119fdafa883
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-08-26 23:15:09 +0530

    I'll never forget this day spent 2 fkin day ruined my weekend just becoz of a if-else

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 //Variables
-let firstNumber;
+let firstNumber = "";
 let possibleOperators = ["+", "-", "*", "/"];
 let operator;
-let secondNumber;
+let secondNumber = "";
+let waitingForSecondNumber = false;
+let operatorButtons = document.querySelectorAll(".operator");
+let displayScreen = document.querySelector("#display-p");
+let equalTo = document.querySelector("#equalto");
+let clear = document.querySelector("#clear");
+let result;
 
 //functions for calculations
 function add(param1, param2) {
   result = param1 + param2;
+  displayScreen.textContent = result;
   console.log(result);
 }
 function subtract(param1, param2) {
   result = param1 - param2;
+  displayScreen.textContent = result;
   console.log(result);
 }
 function multiply(param1, param2) {
   result = param1 * param2;
+  displayScreen.textContent = result;
   console.log(result);
 }
 function divide(param1, param2) {
   result = param1 / param2;
+  displayScreen.textContent = result;
   console.log(result);
 }
+function handleOperator() {
+  operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (firstNumber != "" && secondNumber != "") {
+        operate(firstNumber, operator, secondNumber);
+        firstNumber = result;
+        secondNumber = "";
+        displayScreen.textContent = result;
+      }
+      operator = button.textContent;
+      waitingForSecondNumber = true;
+    });
+  });
+}
+
 function operate(firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
   if (possibleOperators.includes(operator)) {
     if (operator == "+") {
       add(firstNumber, secondNumber);
@@ -37,3 +64,34 @@ function operate(firstNumber, operator, secondNumber) {
     return;
   }
 }
+function populate() {
+  let allNumberButtons = document.querySelectorAll(".numbers");
+  allNumberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (!waitingForSecondNumber) {
+        if (displayScreen.textContent == "Clear") {
+          displayScreen.textContent = "";
+        }
+        firstNumber += button.textContent;
+        displayScreen.textContent = firstNumber;
+      } else {
+        secondNumber += button.textContent;
+        displayScreen.textContent = secondNumber;
+      }
+    });
+  });
+}
+clear.addEventListener("click", () => {
+  displayScreen.textContent = "Clear";
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  waitingForSecondNumber = false;
+});
+equalTo.addEventListener("click", () => {
+  if (firstNumber && operator && secondNumber) {
+    operate(firstNumber, operator, secondNumber);
+  }
+});
+populate();
+handleOperator();
