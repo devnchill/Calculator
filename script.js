febcdeb25d667746023627e2bb13fbeaeea712cf
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-08-27 00:33:18 +0530

    Almost done now only backspace button is left

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Variables
 let firstNumber = "";
-let possibleOperators = ["+", "-", "*", "/"];
+let possibleOperators = ["+", "%", "-", "*", "/"];
 let operator;
 let secondNumber = "";
 let waitingForSecondNumber = false;
@@ -8,32 +8,57 @@ let operatorButtons = document.querySelectorAll(".operator");
 let displayScreen = document.querySelector("#display-p");
 let equalTo = document.querySelector("#equalto");
 let clear = document.querySelector("#clear");
+let point = document.querySelector("#point");
 let result;
+let activeOperatorButton = null;
 
 //functions for calculations
+function roundToTwo(param) {
+  return Math.round(param * 100) / 100;
+}
 function add(param1, param2) {
-  result = param1 + param2;
+  result = roundToTwo(param1 + param2);
+  displayScreen.textContent = result;
+  console.log(result);
+}
+function remainder(param1, param2) {
+  result = roundToTwo(param1 % param2);
   displayScreen.textContent = result;
   console.log(result);
 }
 function subtract(param1, param2) {
-  result = param1 - param2;
+  result = roundToTwo(param1 - param2);
   displayScreen.textContent = result;
   console.log(result);
 }
 function multiply(param1, param2) {
-  result = param1 * param2;
+  result = roundToTwo(param1 * param2);
   displayScreen.textContent = result;
   console.log(result);
 }
 function divide(param1, param2) {
-  result = param1 / param2;
+  if (param2 == 0) {
+    alert(
+      "Seems like you skipped your middleschool . No Problem I'll teach you . If you divide a no by 0 it'll won't be defined.",
+    );
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    populate();
+    handleOperator();
+  }
+  result = roundToTwo(param1 / param2);
   displayScreen.textContent = result;
   console.log(result);
 }
 function handleOperator() {
   operatorButtons.forEach((button) => {
     button.addEventListener("click", () => {
+      if (activeOperatorButton) {
+        activeOperatorButton.style.background = "";
+      }
+      button.style.background = "lightblue";
+      activeOperatorButton = button;
       if (firstNumber != "" && secondNumber != "") {
         operate(firstNumber, operator, secondNumber);
         firstNumber = result;
@@ -47,8 +72,8 @@ function handleOperator() {
 }
 
 function operate(firstNumber, operator, secondNumber) {
-  firstNumber = parseInt(firstNumber);
-  secondNumber = parseInt(secondNumber);
+  firstNumber = parseFloat(firstNumber);
+  secondNumber = parseFloat(secondNumber);
   if (possibleOperators.includes(operator)) {
     if (operator == "+") {
       add(firstNumber, secondNumber);
@@ -68,6 +93,11 @@ function populate() {
   let allNumberButtons = document.querySelectorAll(".numbers");
   allNumberButtons.forEach((button) => {
     button.addEventListener("click", () => {
+      if (activeOperatorButton) {
+        activeOperatorButton.style.background = "";
+        activeOperatorButton = null;
+      }
+
       if (!waitingForSecondNumber) {
         if (displayScreen.textContent == "Clear") {
           displayScreen.textContent = "";
@@ -80,6 +110,19 @@ function populate() {
       }
     });
   });
+  point.addEventListener("click", () => {
+    if (!waitingForSecondNumber) {
+      if (!firstNumber.includes(".")) {
+        firstNumber += ".";
+        displayScreen.textContent = firstNumber;
+      }
+    } else {
+      if (!secondNumber.includes(".")) {
+        secondNumber += ".";
+        displayScreen.textContent = secondNumber;
+      }
+    }
+  });
 }
 clear.addEventListener("click", () => {
   displayScreen.textContent = "Clear";
@@ -91,6 +134,10 @@ clear.addEventListener("click", () => {
 equalTo.addEventListener("click", () => {
   if (firstNumber && operator && secondNumber) {
     operate(firstNumber, operator, secondNumber);
+  } else {
+    alert("Invalid Input. Enter again");
+    populate();
+    handleOperator();
   }
 });
 populate();
